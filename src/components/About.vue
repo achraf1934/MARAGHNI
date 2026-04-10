16ff68409ab2ec95c9eb7a28a777681b4c8fba6b
achraf1934 <achraf.zarroug96@gmail.com>
2026-04-10 18:04:44 +0100

    firstf

--- src/components/About.vue
+++ src/components/About.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useScrollReveal } from '../composables/useScrollReveal';
 
 useScrollReveal();
